5f156fe9a6a7714cd7f814cb0d038d9e796300a2
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-12 10:18:55 -0500

    added check for being loggedin, added request to server for auth

--- src/App.js
+++ src/App.js
@@ -6,17 +6,46 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    loggedIn: false
+    loggedIn: false,
+    authToken: ""
+  };
+
+  componentDidMount() {
+    this.checkForToken();
+  }
+
+  checkForToken = () => {
+    if (sessionStorage.token) {
+      console.log("token found");
+      this.setState({ loggedIn: true, authToken: sessionStorage.token });
+    } else {
+      this.setState({ loggedIn: false });
+    }
   };
 
   signIn = (email, password) => {
-    console.log(email, password);
+    fetch("//localhost:5000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    })
+      .then(res => res.json())
+      .then(response =>
+        this.setState(
+          { loggedIn: true, authToken: response.code },
+          () => (sessionStorage.token = response.code)
+        )
+      )
+      .catch(e => console.log(e));
+    // console.log(email, password);
   };
 
   render() {
     return (
       <div className="App">
-        {this.state.loggedIn ? null : <Redirect to="/login" />}
+        {this.state.loggedIn ? <Redirect to="/" /> : <Redirect to="/login" />}
 
         <Route
           path="/login"
