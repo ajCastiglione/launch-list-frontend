47c1fde0fe0f92c3db997dcdd4da648cce4870ab
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-15 16:44:13 -0500

    added textfields to accomodate new List schema, updated response handling to pass off needed info to display msg

--- src/pages/AddList.js
+++ src/pages/AddList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+// Display messages
+import MySnackBar from "./../displayMessages/MySnackBar";
 // UI Lib
 import { withStyles } from "@material-ui/core/styles";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
@@ -9,11 +11,16 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
 const styles = theme => ({
   root: {
     display: "flex",
     flexWrap: "wrap"
+  },
+  margin: {
+    margin: "10px auto",
+    "text-transform": "capitalize"
   },
   formControl: {
     margin: theme.spacing.unit,
@@ -21,13 +28,24 @@ const styles = theme => ({
   },
   selectEmpty: {
     marginTop: theme.spacing.unit * 2
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
   }
 });
 
 class AddList extends Component {
   state = {
     listType: "",
-    labelWidth: 0
+    listName: "",
+    associatedClient: "",
+    labelWidth: 0,
+    listID: null,
+    success: false,
+    successMsg: "",
+    warning: false,
+    warningMsg: ""
   };
 
   componentDidMount() {
@@ -36,8 +54,44 @@ class AddList extends Component {
     });
   }
 
+  closeModal = () => this.setState({ warning: false, success: false });
+
   handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+      warning: false,
+      success: false
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let { listType } = this.state;
+    if (!listType)
+      return this.setState({ warning: true, warningMsg: "Enter a type" });
+    fetch("//localhost:5000/lists", {
+      method: "POST",
+      headers: {
+        "x-auth": sessionStorage.token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ type: listType })
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          throw Error("Authenticated failed.");
+        }
+      })
+      .then(res => {
+        this.setState({
+          success: true,
+          successMsg: "List created successfully!",
+          listID: res._id
+        });
+      })
+      .catch(e => console.error(e));
   };
 
   render() {
@@ -47,11 +101,25 @@ class AddList extends Component {
         <h1 className="add-list-title">Generate new list</h1>
         <section className="section-container add-list large-wrapper">
           <form className="add-list-form" action="">
+            {this.state.warning ? (
+              <MySnackBar
+                variant="warning"
+                className={classes.margin}
+                message={this.state.warningMsg}
+                onClick={this.closeModal}
+              />
+            ) : this.state.success ? (
+              <MySnackBar
+                variant="success"
+                className={classes.margin}
+                message={this.state.successMsg}
+                target={this.state.listName}
+                onClick={this.closeModal}
+              />
+            ) : null}
             <Grid container spacing={24} className="form-grid">
               <Grid className="grid-left" item xs={12} md={6} lg={5}>
-                <h2 className="add-list-subtitle">
-                  Choose the type of list you would like to generate
-                </h2>
+                <h2 className="add-list-subtitle">select the type of list:</h2>
                 <FormControl variant="outlined" className={classes.formControl}>
                   <InputLabel
                     ref={ref => {
@@ -75,19 +143,46 @@ class AddList extends Component {
                     <MenuItem value="">
                       <em>None</em>
                     </MenuItem>
-                    <MenuItem value="todo">Todo List</MenuItem>
-                    <MenuItem value="launch">Launch List</MenuItem>
-                    <MenuItem value="live">Live List</MenuItem>
-                    <MenuItem value="ecom">Ecommerce List</MenuItem>
-                    <MenuItem value="ecom-live">Ecommerce Live List</MenuItem>
+                    <MenuItem value="todo-list">Todo List</MenuItem>
+                    <MenuItem value="launch-list">Launch List</MenuItem>
+                    <MenuItem value="live-list">Live List</MenuItem>
+                    <MenuItem value="ecom-list">Ecommerce List</MenuItem>
+                    <MenuItem value="ecom-live-list">
+                      Ecommerce Live List
+                    </MenuItem>
                   </Select>
                 </FormControl>
+                {this.state.listType === "todo-list" ? (
+                  <React.Fragment>
+                    <h2 className="add-list-subtitle">List Name:</h2>
+                    <TextField
+                      label="List Name"
+                      name="listName"
+                      className={`${classes.textField} input-txt-container`}
+                      value={this.state.listName}
+                      onChange={this.handleChange}
+                      placeholder="List Name"
+                      margin="normal"
+                    />
+                  </React.Fragment>
+                ) : null}
+                <h2 className="add-list-subtitle">Website Name:</h2>
+                <TextField
+                  label="Website"
+                  name="website"
+                  className={`${classes.textField} input-txt-container`}
+                  value={this.state.website}
+                  onChange={this.handleChange}
+                  placeholder="Website"
+                  margin="normal"
+                />
               </Grid>
               <Grid className="grid-right" item xs={12} md={6} lg={5}>
+                <h2 className="add-list-subtitle">submit new list</h2>
                 <Button
                   variant="outlined"
                   color="primary"
-                  onClick={this.submitNewList}
+                  onClick={this.handleSubmit}
                 >
                   Create List
                 </Button>
