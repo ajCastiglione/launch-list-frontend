3d889085e7f5723b6f2df7eb4be5432c5c723754
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-15 23:07:13 -0500

    fetch setup and able to add lists to their designated arrays in state, working on displaying them and if I am going to break this down into sub components due to clutter of the page and DRY method

--- src/pages/AllLists.js
+++ src/pages/AllLists.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class AllLists extends Component {
+  state = {
+    todo_list: [],
+    launch_list: [],
+    live_list: [],
+    ecom_list: [],
+    ecom_live_list: [],
+    listsAcquired: false
+  };
+
+  componentDidMount() {
+    this.getLists();
+  }
+
+  getLists = () => {
+    fetch("//localhost:5000/lists", {
+      headers: {
+        "x-auth": sessionStorage.token
+      }
+    })
+      .then(res =>
+        res.status === 200
+          ? res.json()
+          : new Error("Could not authenticate and acquire lists.")
+      )
+      .then(res => {
+        res.lists.map((el, idx) => {
+          let temp = this.state[el.type];
+          temp.push(el);
+          return this.setState({ [el.type]: temp });
+        });
+        this.setState({ listsAcquired: true });
+      })
+      .catch(e => console.error(e));
+  };
+
   render() {
     return (
       <main className="main">
@@ -10,12 +45,17 @@ class AllLists extends Component {
             <div className="list-container">
               <h2 className="list-section-title">Todo Lists</h2>
               <div className="active-lists">
-                <p className="empty-lists">
-                  Looks like there aren't any lists to display for this section.{" "}
-                  <Link to="/add-list" className="link-btn">
-                    Go here to generate a new list!
-                  </Link>
-                </p>
+                {this.state.listsAcquired ? (
+                  this.state.todo_list.map((el, idx) => <h2>{el.listName}</h2>)
+                ) : (
+                  <p className="empty-lists">
+                    Looks like there aren't any lists to display for this
+                    section.{" "}
+                    <Link to="/add-list" className="link-btn">
+                      Go here to generate a new list!
+                    </Link>
+                  </p>
+                )}
               </div>
             </div>
           </section>
