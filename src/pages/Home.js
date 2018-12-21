7ed2d20b763aeacf76d32af676f70fe6fdc46e73
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-20 19:06:20 -0500

    reworked layout, added all necessary logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,76 +1,192 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+// UI lib
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
+const styles = {
+  card: {
+    maxWidth: 345,
+    margin: "auto",
+    height: "100%"
+  },
+  media: {
+    height: 140
+  }
+};
+
 class Home extends Component {
+  state = {
+    all_lists: [],
+    todo_list: [],
+    launch_list: [],
+    live_list: [],
+    ecom_list: [],
+    ecom_live_list: [],
+    listsAcquired: false,
+    listType: ""
+  };
+
+  componentDidMount() {
+    this.getLists();
+  }
+
+  getLists = () => {
+    fetch("//localhost:5000/lists", {
+      headers: {
+        "x-auth": sessionStorage.token
+      }
+    })
+      .then(res =>
+        res.status === 200
+          ? res.json()
+          : new Error("Could not authenticate and acquire lists.")
+      )
+      .then(res => {
+        res.lists.map((el, idx) => {
+          let temp = this.state[el.type];
+          temp.push(el);
+          if (temp.length >= 5) temp = temp.slice(0, 5);
+          return this.setState({ [el.type]: temp });
+        });
+
+        let allStateLists = [
+          {
+            title: "Todo Lists",
+            array: this.state.todo_list,
+            type: "todo_list",
+            img: "https://s3.amazonaws.com/minervalists/todo-list.jpg"
+          },
+          {
+            title: "Launch Lists",
+            array: this.state.launch_list,
+            type: "launch_list",
+            img: "https://s3.amazonaws.com/minervalists/launch-list.png"
+          },
+          {
+            title: "Live Lists",
+            array: this.state.live_list,
+            type: "live_list",
+            img: "https://s3.amazonaws.com/minervalists/live-list.png"
+          },
+          {
+            title: "Ecommerce Lists",
+            array: this.state.ecom_list,
+            type: "ecom_list",
+            img: "https://s3.amazonaws.com/minervalists/ecom-list.png"
+          },
+          {
+            title: "Ecommerce Live Lists",
+            array: this.state.ecom_live_list,
+            type: "ecom_live_list",
+            img: "https://s3.amazonaws.com/minervalists/ecom-live-list.jpg"
+          }
+        ];
+        this.setState({ listsAcquired: true, all_lists: allStateLists });
+      })
+      .catch(e => console.error(e));
+  };
+
+  countCompleted = array => {
+    let totalCompleted = 0;
+    array.map(el => {
+      if (el.completed) return totalCompleted++;
+      return false;
+    });
+    return totalCompleted;
+  };
+
+  handleMO = type => this.props.updateListType(type);
+
   render() {
+    const { classes } = this.props;
+    const { listsAcquired, all_lists } = this.state;
+    const cards = listsAcquired ? (
+      all_lists.map((lists, lIndex) => (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          xl={2}
+          key={`list-${lists.title}-${lIndex}`}
+        >
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={lists.img}
+                title="List"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {lists.title}
+                </Typography>
+                <Typography component="div">
+                  <p>
+                    Number of {lists.title}: {lists.array.length}
+                  </p>
+                  <p>Completed: {this.countCompleted(lists.array)}</p>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                className="btn-container"
+                variant="outlined"
+                size="small"
+                color="primary"
+              >
+                <Link
+                  to={`/lists/${lists.type}s`}
+                  onMouseOver={() => this.handleMO(lists.type)}
+                  className="btn"
+                >
+                  View All
+                </Link>
+              </Button>
+
+              <Button
+                className="btn-container"
+                variant="outlined"
+                size="small"
+                color="primary"
+              >
+                <Link to="/add-list" className="btn">
+                  Create More
+                </Link>
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))
+    ) : (
+      <div className="spinner" />
+    );
+
     return (
-      <React.Fragment>
-        <article className="home-article large-wrapper">
-          <section className="section-container todo-section">
-            <div className="list-container">
-              <h2 className="list-section-title">Todo Lists</h2>
-              <div className="active-lists">
-                <p className="empty-lists">
-                  Looks like there aren't any lists to display for this section.{" "}
-                  <Link to="/add-list" className="link-btn">
-                    Go here to generate a new list!
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </section>
-          <section className="section-container live-section">
-            <div className="list-container">
-              <h2 className="list-section-title">launch Lists</h2>
-              <div className="active-lists">
-                <p className="empty-lists">
-                  Looks like there aren't any lists to display for this section.{" "}
-                  <Link to="/add-list" className="link-btn">
-                    Go here to generate a new list!
-                  </Link>
-                </p>
-              </div>
-            </div>
-            <div className="list-container">
-              <h2 className="list-section-title">live Lists</h2>
-              <div className="active-lists">
-                <p className="empty-lists">
-                  Looks like there aren't any lists to display for this section.{" "}
-                  <Link to="/add-list" className="link-btn">
-                    Go here to generate a new list!
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </section>
-          <section className="section-container ecom-section">
-            <div className="list-container">
-              <h2 className="list-section-title">ecommerce Lists</h2>
-              <div className="active-lists">
-                <p className="empty-lists">
-                  Looks like there aren't any lists to display for this section.{" "}
-                  <Link to="/add-list" className="link-btn">
-                    Go here to generate a new list!
-                  </Link>
-                </p>
-              </div>
-            </div>
-            <div className="list-container">
-              <h2 className="list-section-title">ecommerce live Lists</h2>
-              <div className="active-lists">
-                <p className="empty-lists">
-                  Looks like there aren't any lists to display for this section.{" "}
-                  <Link to="/add-list" className="link-btn">
-                    Go here to generate a new list!
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </section>
-        </article>
-      </React.Fragment>
+      <article className="home-article">
+        <h1 className="article-title">Home</h1>
+        <section className="section-container large-wrapper">
+          <h2>All Lists</h2>
+          <Divider />
+          <Grid container spacing={24} className="grid-container">
+            {cards}
+          </Grid>
+        </section>
+      </article>
     );
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
