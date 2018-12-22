ef7a420b7ef0acefd704be4f1f67785b2d354bdc
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-22 17:36:00 -0500

    Updates

---
+++ precache-manifest.28ce10dab1b6f98a620c63de6f6440dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec3271b8c3632eaa5b573c434e9f2303",
+    "url": "/static/media/logo.ec3271b8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e14be08f6b54fe47d3f",
+    "url": "/static/js/main.0e14be08.chunk.js"
+  },
+  {
+    "revision": "702e1214beb9c9d578f1",
+    "url": "/static/js/1.702e1214.chunk.js"
+  },
+  {
+    "revision": "0e14be08f6b54fe47d3f",
+    "url": "/static/css/main.b55916d6.chunk.css"
+  },
+  {
+    "revision": "31f6f9592fc1b1e7d6f247ba57c45914",
+    "url": "/index.html"
+  }
+];
