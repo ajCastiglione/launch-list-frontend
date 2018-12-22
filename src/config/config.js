a9420fc5398cf6fa05d17f9d1d51a035aac4b230
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-12-22 18:04:42 -0500

    fixed url typo for live

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ let url;
 if (window.location.href.includes("localhost")) {
   url = "localhost:5000";
 } else {
-  url = "https://minerva-checklist.herokuapp.com/";
+  url = "https://minerva-checklist.herokuapp.com";
 }
 
 export default url;
